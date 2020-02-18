115e06cb6f5e37c6fe2bb2041d34ebfa11de9cd1
RajNSankhe <sankhe.r@husky.neu.edu>
2020-02-17 20:17:25 -0500

    cicd setup

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo 'cleaning cache'
-sudo yum clean all
-sudo yum makecache
 echo 'installing Open jdk 8'
 cd ~
-sudo yum install wget -y
-sudo yum install java-1.8.0-openjdk -y
+sudo apt-get update
+sudo apt install openjdk-8-jdk
 java -version
 echo 'installed java successfully.'
+echo 'installing tomcat'
+sudo apt-get install tomcat8 tomcat8-docs tomcat8-examples tomcat8-admin apache2 -y
+echo 'installed tomcat successfully.'
